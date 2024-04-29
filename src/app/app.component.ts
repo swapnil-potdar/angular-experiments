9b5409f719ea4ca0714b66c67ede1c15dd8fcfdc
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-04-29 17:25:05 +0530

    started with observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,12 +10,15 @@ export class AppComponent implements OnInit {
   title = 'angular-experiments';
 
   myObservable = new Observable((observer) => {
-    console.log('observable starts');
+    var a = 10;
+    var b = 5;
+    var c = a + b;
+    console.log(c);
   });
 
   ngOnInit() {
-    this.myObservable.subscribe((val) => {
-      console.log(val);
+    this.myObservable.subscribe((c) => {
+      console.log(c);
     });
   }
 }
