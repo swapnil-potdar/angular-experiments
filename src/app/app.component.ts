af6780d13d98468030359a2f396a3d0154ff09d0
Swapnil Potdar <potdar.swapnil10@gmail.com>
2024-04-29 17:18:01 +0530

    studying observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-experiments';
+
+  myObservable = new Observable((observer) => {
+    console.log('observable starts');
+  });
+
+  ngOnInit() {
+    this.myObservable.subscribe((val) => {
+      console.log(val);
+    });
+  }
 }
